5a8ffd0ec21e36f81b80c0b275b5b4c62237dd57
niall-turbitt <niall.turbitt@databricks.com>
2025-12-02 11:48:35 -0800

    fix old import bug

--- telco_support_agent/ops/__init__.py
+++ telco_support_agent/ops/__init__.py
@@ -2,14 +2,14 @@
 
 from .deployment import cleanup_old_deployments, deploy_agent
 from .logging import log_agent
-from .monitoring import setup_agent_scorers
+from .monitoring import setup_production_monitoring
 from .registry import get_latest_model_version, register_agent_to_uc
 
 __all__ = [
     "deploy_agent",
     "cleanup_old_deployments",
     "log_agent",
-    "setup_agent_scorers",
+    "setup_production_monitoring",
     "register_agent_to_uc",
     "get_latest_model_version",
 ]
